550b31eadc0bc62d63136c85333c2c5ca0f42f0e
anah <40556052+z-anah@users.noreply.github.com>
2024-01-22 21:31:35 +0700

    firebase

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,71 @@
-<script setup lang="ts"></script>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+// firebase
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  onSnapshot,
+  collection,
+  doc,
+  deleteDoc,
+  setDoc,
+  addDoc,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import {ref,onUnmounted} from 'vue';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQQdDLImj_2kxfOUUlBLhITt2Z682d95k",
+  authDomain: "perso-news-app.firebaseapp.com",
+  projectId: "perso-news-app",
+  storageBucket: "perso-news-app.appspot.com",
+  messagingSenderId: "906006395441",
+  appId: "1:906006395441:web:a03165775f4621fe123310",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+
+
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  methods: {
+  },
+  data:()=> {
+    return {
+      messages: [] as { id: string; label: string; harga: string }[],
+    }
+  },
+  mounted() {
+    console.log("mounted");
+    
+    const latestQuery = query(collection(db, "ps_menu"), orderBy("label", "desc"));
+    const livemessages = onSnapshot(latestQuery,(snapshot)=>{
+      this.$data.messages = snapshot.docs.map((doc) => {
+        console.log(doc);
+        
+        return {
+          ...doc.data(),
+          id:doc.id,
+          harga:doc.data().harga,
+          label:doc.data().label
+        }
+      });
+    });
+    onUnmounted(livemessages)
+    console.log(this.$data.messages);
+    
+  }
+});
+
+</script>
 
 <template>
   <p>
@@ -6,7 +73,12 @@
     <router-link to="/about">Go to About</router-link>
   </p>
   <router-view></router-view>
+  <!-- list messages -->
+  <ul>
+    <li v-for="item in messages" :key="item.id">
+    {{ item.label }} - {{ item.harga }}
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
