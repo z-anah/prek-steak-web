150f0c086ccce1118cda26236c1a237d5be4cb15
anah <40556052+z-anah@users.noreply.github.com>
2024-01-22 17:49:44 +0700

    router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import Test from "./pages/Test.vue";
+import Test2Vue from "./pages/Test2.vue";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/", component: Test },
+  { path: "/about", component: Test2Vue },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
---
+++ src/pages/Test2.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>TEST2</p>
+</template>
+
+<style scoped></style>
